942e7126f86722530fe35d779e47be83bd1e0b3c
Andrew Piispanen <apiispanen1@babson.edu>
2023-05-11 11:17:22 -0400

    host IP

--- SpinnrAIWebService/classification.py
+++ SpinnrAIWebService/classification.py
@@ -1,20 +1,19 @@
-import spacy
+import spacy # NLP processing
+from spacy.matcher import Matcher
 nlp = spacy.load("en_core_web_sm")
-
+import numpy as np
+import pandas as pd
+# IMPORT THE ML LIBRARIES
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-
-
-import pandas as pd
 from sklearn.model_selection import train_test_split
-from spacy.matcher import Matcher
 
 matcher = Matcher(nlp.vocab)
 
-# Define a simple pattern for interests
+# Define a simple pattern for our custom entity of "interests"
 pattern = [{"LOWER": {"IN": ["hiking", "camping", "cooking", "technology", "ai", "painting", "writing", "reading", "meditating", "backpacking", "jogging"]}}]
 
 # Add the pattern to the matcher
@@ -25,8 +24,6 @@ def extract_entities(text):
     entities = {}
     for ent in doc.ents:
         entities[ent.label_] = ent.text
-
-    # Find the interests using the matcher
     matches = matcher(doc)
     if matches:
         _, start, end = matches[0]
@@ -38,10 +35,10 @@ def extract_entities(text):
 def generate_query(intent, entities):
     intent = intent.lower()
     if intent == "events":
-        location = entities.get("GPE")
+        location = entities.get("GPE") # GPE is geopolitical entity
         query = f"SELECT * FROM events WHERE location = '{location}'"
     elif intent == "people":
-        interests = entities.get("INTERESTS")
+        interests = entities.get("INTERESTS") # This is our custom entity "interests"
         query = f"SELECT * FROM people WHERE interests = '{interests}'"
     else:
         query = ""
@@ -53,6 +50,7 @@ data = pd.read_csv("intent_data.csv")
 X_train, X_test, y_train, y_test = train_test_split(data["sentence"], data["intent"], test_size=0.2, random_state=42, stratify=data["intent"])
 
 
+# TRAIN THE MODEL
 pipeline = Pipeline([
     ('tfidf', TfidfVectorizer()),
     ('classifier', LogisticRegression())
@@ -62,6 +60,14 @@ pipeline.fit(X_train, y_train)
 
 y_pred = pipeline.predict(X_test)
 print(classification_report(y_test, y_pred))
+
+# print the shape of the pipeline model
+print(pipeline.named_steps['tfidf'].transform(X_train).shape)
+
+
+# Assuming y_train is a 1D numpy array
+print(np.array(y_train).shape)
+
 
 def predict_intent(text):
     return pipeline.predict([text])[0]
@@ -82,7 +88,7 @@ def query_intent(text):
     intent = predict_intent(text) # Can be done with the NLP cloud API
     entities = extract_entities(text)
     query = generate_query(intent, entities)
-    # results = execute_query(query)
+    # results = execute_query(query) # ONCE WE HOOK UP THE DB THIS IS WHERE WE'LL EXECUTE THE QUERY
     return query
 
 
